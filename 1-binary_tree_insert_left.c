9042888a2306f50bf806d8b8db2c2ae16796f104
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-25 16:45:06 +0100

    tast 1 ag

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,22 +5,26 @@
  * @value: Value integer
  * Return: a pointer to the created node, or NULL on failure
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
-	if (parent == NULL)
+	if (!parent)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
 		return (NULL);
 
-	if (parent->left != NULL)
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->right = NULL;
+	if (parent->left)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
+
 	return (new_node);
 }
