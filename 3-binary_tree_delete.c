61054541ee69946b76c27ac5020469303512b7b5
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-25 21:48:43 +0100

    task 3 ag

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,15 +7,19 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
+	if (!tree)
 		return;
-	}
-	else
+
+	if (tree->left)
 	{
 		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-
-		free(tree);
+		tree->left = NULL;
 	}
+	if (tree->right)
+	{
+		binary_tree_delete(tree->right);
+		tree->right = NULL;
+	}
+	if (!tree->left && !tree->right)
+		free(tree);
 }
