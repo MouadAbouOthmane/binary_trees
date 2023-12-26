840a2e8af4c6956a9f5871fd01122a412180da48
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-26 22:47:26 +0100

    task 12 ag

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: node
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (!node->left && !node->right)
+		return (1);
+
+	return (0);
+}
+
+
+/**
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: the root node
  * Return: 0 If tree is NULL.
